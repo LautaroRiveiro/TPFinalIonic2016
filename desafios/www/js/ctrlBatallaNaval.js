b2a0a9d43847981507f969f6f268ec0b98034d83
Lautaro Riveiro <lautaroriveiro91@gmail.com>
2016-12-25 19:36:33 -0300

    Batalla Naval
    Esta actualización puede fallar xD
    - Agregué una pantalla inicial con un resumen de las partidas creadas y
    no aceptadas, las partidas que me toca jugar, y las que le toca jugar al
    oponente.
    - Agregué el sistema de actualizar los turnos a medida que voy jugando.

--- desafios/www/js/ctrlBatallaNaval.js
+++ desafios/www/js/ctrlBatallaNaval.js
@@ -6,7 +6,15 @@ angular.module('batallaNaval.controller', [])
   $scope.bandera = {};
   $scope.bandera.estado = "inicio";
   $scope.partidas = {};
-  //$scope.partidasPropias = [];
+  $scope.partidasPropiasEsperando = {};
+  $scope.partidasEnJuego = {};
+  $scope.turno = {};
+  $scope.turno.turno = firebase.auth().currentUser.uid;
+  $scope.mostrarRadio = {};
+  $scope.mostrarRadio._1 = false;
+  $scope.mostrarRadio._2 = false;
+  $scope.mostrarRadio._3 = false;
+  $scope.mostrarRadio._4 = false;
 
   //Referencio a Partidas de Firebase
   var refPartidas = new Firebase("https://tpfinalionic2016.firebaseio.com/partidas");
@@ -17,6 +25,12 @@ angular.module('batallaNaval.controller', [])
       if(partida.creadorUid != firebase.auth().currentUser.uid && partida.estado == "esperando"){
           //$scope.partidas.push(partida);  
           $scope.partidas[partida.key] = partida;
+      }
+      else if (partida.creadorUid == firebase.auth().currentUser.uid && partida.estado == "esperando"){
+          $scope.partidasPropiasEsperando[partida.key] = partida;
+      }
+      if (partida.estado != "esperando" && (partida.creadorUid == firebase.auth().currentUser.uid || partida.desafianteUid == firebase.auth().currentUser.uid)) {
+          $scope.partidasEnJuego[partida.key] = partida;
       }
       console.info("Partidas nueva: ", $scope.partidas);
     });
@@ -30,7 +44,14 @@ angular.module('batallaNaval.controller', [])
           //$scope.partidas.splice(partida.key, 1);
           delete $scope.partidas[partida.key];
       }
+      if (partida.creadorUid == firebase.auth().currentUser.uid && partida.estado != "esperando"){
+          delete $scope.partidasPropiasEsperando[partida.key];
+      }
+      if (partida.estado != "esperando" && (partida.creadorUid == firebase.auth().currentUser.uid || partida.desafianteUid == firebase.auth().currentUser.uid)) {
+          $scope.partidasEnJuego[partida.key] = partida;
+      }
       console.info("Partidas modif: ", $scope.partidas);
+      console.info("Partidas Propias modif: ", $scope.partidasPropias);
     });
   });
 
@@ -87,7 +108,8 @@ angular.module('batallaNaval.controller', [])
         desafianteUid: firebase.auth().currentUser.uid,
         ubicacionDesafiante: $scope.partida.ubicacionDesafiante,
         estado: "aceptado",
-        fechaAceptado: Firebase.ServerValue.TIMESTAMP
+        fechaAceptado: Firebase.ServerValue.TIMESTAMP,
+        turno: firebase.auth().currentUser.uid
       });
       //Reinicio valores
       //$scope.key = "";
@@ -119,6 +141,81 @@ angular.module('batallaNaval.controller', [])
     }
     console.info("KEY: ", $scope.key);
     //Vuelvo a las partidas creadas
+    $scope.bandera.estado = "buscar";
+  };
+
+  //---------------------------- TU TURNO -----------------------------------//
+  /* Cuando selecciono una de las partidas que tengo turno, hago lo siguiente:
+   * 1. Clono (NO referencio) la partida y sus descendientes, o sea los turnos, a una variable $scope.partida
+   * 2. Habilito la vista (estado) para elegir ubicación
+   * 3. Guardo la key de la partida en una variable para luego updatear la jugada en Firebase
+   * 4. Disableo las opciones donde ya jugué y no acerté
+   */
+  $scope.JugarTurno = function(partida){
+    $scope.partida = jQuery.extend(true, {}, partida);
+    $scope.bandera.estado = 'jugar';
+    $scope.key = $scope.partida.key;
+    $scope.partida.ubicacionDesafiante = "";
+    for (var variableKey in $scope.partida.turnos){
+      if ($scope.partida.turnos[variableKey].jugador == firebase.auth().currentUser.uid){
+          $scope.mostrarRadio["_"+$scope.partida.turnos[variableKey].ubicacion] = true;
+      };
+    };
+  };
+
+  //Cancelar jugada sin elegir
+  $scope.VolverJugada = function(){
+    //Reinicio valores
+    for (var variableKey in $scope.partida){
+        if ($scope.partida.hasOwnProperty(variableKey)){
+            delete $scope.partida[variableKey];
+        }
+    }
+    //Vuelvo al inicio de las partidas
     $scope.bandera.estado = "inicio";
   };
+
+  /* Cuando acepto la jugada, hago lo siguiente:
+   * 1. Valido que esté la ubicación cargada
+   * 2. Identifico el uid del oponente para updatear la partida y poner que le toca al otro
+   * 3. Cambiar datos de la partida existente
+   * 4. Reinicio valores y regreso
+   */
+  $scope.AceptarJugada = function(){
+    //Valido que esté la ubicación cargada
+    if($scope.partida.ubicacionDesafiante != ""){
+      
+      //Identifico el uid del oponente (no se me ocurrió nada mejor)
+      var uidOponente = "";
+      if($scope.partida.creadorUid != firebase.auth().currentUser.uid){
+         uidOponente = $scope.partida.creadorUid;
+      }
+      else{
+         uidOponente = $scope.partida.desafianteUid;
+      };
+      console.info("OPONENTE", uidOponente);
+      
+      //Cambiar datos de la partida existente
+      firebase.database().ref("/partidas/"+$scope.key).update({
+        turno: uidOponente
+      });
+      firebase.database().ref("/partidas/"+$scope.key+"/turnos").push({
+        jugador: firebase.auth().currentUser.uid,
+        fecha: Firebase.ServerValue.TIMESTAMP,
+        ubicacion: $scope.partida.ubicacionDesafiante
+      });
+
+      //Reinicio valores y regreso
+      for (var variableKey in $scope.partida){
+          if ($scope.partida.hasOwnProperty(variableKey)){
+              delete $scope.partida[variableKey];
+          }
+      }
+      $scope.bandera.estado = "inicio";
+    }
+    else{
+      alert("Falta elegir ubicación");
+    }
+  };
+
 });
